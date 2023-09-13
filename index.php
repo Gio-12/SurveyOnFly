a2680963d1be4339139058ae4207e50c7c55ed3c
Gio <83874182+Gio-12@users.noreply.github.com>
2023-09-13 16:28:55 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>SONDAGGI</title>
+  </head>
+<header>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ml-auto">
+
+                <li class="nav-item">
+                    <a class="nav-link" href="login.php">LOGIN</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="registrazione.php">REGISTRATI</a>
+                </li>
+
+            </ul>
+        </div>
+    </nav>
+</header>
+  <body>
+<!--cover, con custom in style.css-->
+<div id="intro" class="view hm-black-strong">
+  <div class="container-fluid full-bg-img d-flex align-items-center justify-content-center">
+      <div class="row d-flex justify-content-center">
+          <div class="col-md-14 text-center">
+              <h2 class="display-3 font-bold text-white text-center nb-2">Benvenuto su  Sondaggi</h2>
+              <hr class="hr-light">
+              <h4 class="text-white text-center">Piattaforma per fare Sondaggi
+              </h4>
+          </div>
+      </div>
+  </div>
+</div>
+  </body>
+</html>
